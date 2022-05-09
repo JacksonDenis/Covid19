b8f073dcfcc09f10014fb7687e9868b801b70cce
Jackson Denis <jackson.denis@live.com>
2022-05-09 14:25:02 -0300

    Projeto Covid PWA INIT

---
+++ src/containers/Main/index.jsx
@@ -0,0 +1,44 @@
+import React,  {memo, useState, useCallback, useEffect} from "react";
+import Api from '../../api';
+import Board from './Components/Board';
+import Panel from './Components/Panel';
+import {containerStyled} from './style'
+
+function Main() {
+    const [data,setData] = useState({})
+    const [country, setCountry] = useState('brazil') 
+    const updateAt = new Date().toLocaleString()
+
+    const getCovitData = useCallback((country) => {
+        Api.getCountry(country)
+            .then(data=> setData(data))
+    },[])
+
+    useEffect (()=> {
+        getCovitData(country)
+    },[getCovitData, country])
+
+    const handleChange = ({target}) => {
+        const country = target.value 
+        setCountry(country)
+    }
+
+    return(
+        <containerStyled>
+            <div className="mb-2">
+                <Panel
+                data={data}
+                updataAt={updateAt}
+                onChange={handleChange}
+                country={country}
+                getCovitData={getCovitData}                
+                />
+
+            </div>
+            <Board data={data} />
+        </containerStyled>
+    )
+}
+
+
+export default memo(Main)
---
+++ src/containers/Main/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const containerStyled = styled.div `
+    padding: 20px;
+    margin: 0 auto;
+    max-width: 1024px
+`
